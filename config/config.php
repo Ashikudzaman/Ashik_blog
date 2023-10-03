0501a2304df322870f26a1e9db5072c6485e9524
Ashikudzaman <89346949+Ashikudzaman@users.noreply.github.com>
2023-10-03 15:45:39 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "id21346045_blog022");
+define("DB_PASS", "Ashik123@");
+define("DB_NAME", "id21346045_blog022");
+define("TITLE", "Training With Live Project");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials, Oracle Database, C# Tutorials");
+
+?>
